eb1cf92b1498d20c51c72285016510fb33e609b5
prasanna <prasannaghimire3287@gmail.com>
2025-04-03 11:40:01 +0545

    new approach

---
+++ src/utils/constants.py
@@ -0,0 +1,14 @@
+MONTH_NUMBER_TO_NAME = {
+    "01": "Baisakh", "02": "Jestha", "03": "Ashad", "04": "Shrawan", "05": "Bhadra",
+    "06": "Ashoj", "07": "Kartik", "08": "Mangsir", "09": "Poush", "10": "Magh",
+    "11": "Falgun", "12": "Chaitra"
+}
+
+# Default response structure
+DEFAULT_RESPONSE = {
+    "query": "",
+    "question": "",
+    "graph_keys": [],
+    "desc": "",
+    "data": {}
+}
